debe153932db692bb4d4c3f15a27bb7ad5cb8eb2
BambaDeme <cheikhbambademe@gmail.com>
2020-01-17 00:22:20 +0000

    Ajout des bibliothéques nécessaires et compteur d'images téléchargés

---
+++ dowonload_manga.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import sys
+
+# compteur d'images
+count = 0
+
+print("--------")
+print("--------")
+print("Total images downloaded:", count)
+print("--------")
+print("--------")
