491c9f6d77dd15bb09f03c8c351bea30d12f1485
BambaDeme <cheikhbambademe@gmail.com>
2020-01-17 00:24:39 +0000

    Ajout de fonction nbrePageChapter

--- dowonload_manga.py
+++ dowonload_manga.py
@@ -6,6 +6,19 @@ import sys
 # compteur d'images
 count = 0
 
+def nbrePageChapter(manga,chapter):
+	chapter = str(chapter)
+	web = 'https://www.mangapanda.com/'+manga+'/'+chapter
+	r = requests.get(web)
+	data = r.text
+	soup = BeautifulSoup(data, 'lxml')
+	number = []
+	for link in soup.find_all('option'):
+		number.append(link.get('value').replace('/'+manga+'/'+chapter+'/',''))
+	del number[0]
+	number.insert(0,'1')
+	return number;
+
 print("--------")
 print("--------")
 print("Total images downloaded:", count)
